7b260aebbd9f6f8cb9b6fb2fd797f568716e0482
Файрушин Ян <fajrusinan6@gmail.com>
2026-09-11 00:38:36 +0500

    This function will be used to display the results of the quiz game. It will take in the user's score and the total number of questions, and return a string that displays the user's score as a percentage.

--- js/objects/quizGame.js
+++ js/objects/quizGame.js
@@ -5,5 +5,43 @@ const questions = [
   choices: ['1','2','3'],
   answer: '1'
 
+  },
+  {
+    category: "science",
+    question: "What is the powerhouse of the cell?",
+    choices: ["Mitochondria", "Nucleus", "Ribosome"],
+    answer: "Mitochondria"
+  },
+  {
+    category: "geography",
+    question: "What is the capital of France?",
+    choices: ["London", "Berlin", "Paris"],
+    answer: "Paris"
+  },
+  {
+    category: "math",
+    question: "What is 2 + 2?",
+    choices: ["3", "4", "5"],
+    answer: "4"
+  },
+  {
+    category: "history",
+    question: "Who was the first president of the United States?",
+    choices: ["George Washington", "Abraham Lincoln", "Thomas Jefferson"],
+    answer: "George Washington"
   }
-]
+];
+  
+function getRandomQuestion(questionsArray) {
+  const randomIndex = Math.floor(Math.random() * questionsArray.length);
+  return questionsArray[randomIndex];
+}
+
+function getRandomComputerChoice(choicesArray) {
+  const randomIndex = Math.floor(Math.random() * choicesArray.length);
+  return choicesArray[randomIndex];
+}
+
+function getResults() {
+
+}  
