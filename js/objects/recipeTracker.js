c15055beb15a46b8be85b0c90c8d50fcc4e11cf9
Файрушин Ян <fajrusinan6@gmail.com>
2026-09-10 23:41:48 +0500

    added variables and access objects to them

--- js/objects/recipeTracker.js
+++ js/objects/recipeTracker.js
@@ -23,3 +23,12 @@ const recipe3 = {
   totalIngredients: null,
   difficultyLevel: ""
 };
+
+const recipe1Name = recipe1.name;
+console.log(recipe1Name);
+
+const recipe2CookingTime = recipe2.cookingTime;
+console.log(recipe2CookingTime);
+
+const recipe3Ingredients = recipe3.ingredients;
+console.log(recipe3Ingredients);
